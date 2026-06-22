472096de3b37fc220b6ed578c4f18dab1c2d11ae
Lennox Lewis <lennylewis6a6y@gmail.com>
2026-06-22 15:01:03 +0300

    Social markers on layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,9 +15,38 @@ export const metadata = {
   },
 }
 
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'Organization',
+  'name': 'Zed OS Technologies Limited',
+  'url': 'https://zedostechnologies.co.ke',
+  'founder': {
+    '@type': 'Person',
+    'name': 'Lennox Lewis Odhiambo',
+    'jobTitle': 'CEO & Founder',
+  },
+  'address': {
+    '@type': 'PostalAddress',
+    'addressLocality': 'Kisumu',
+    'addressRegion': 'Kisumu County',
+    'addressCountry': 'KE',
+  },
+  'sameAs': [
+    'https://www.instagram.com/zedostechnologies?igsh=MXFyamJ2NjFvY2F0eQ==',
+    'www.linkedin.com/in/zedos-technologies-576bb3414',
+    'https://x.com/zedostechnology',
+  ],
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
